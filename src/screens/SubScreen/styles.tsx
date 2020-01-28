2f3b6f1fc830ae82cc9eb22ebbf0848e68562ded
Kang Richard <kdilot@Kangs-MacBook-Pro.local>
2020-01-28 09:49:21 +0900

    todo

--- src/screens/SubScreen/styles.tsx
+++ src/screens/SubScreen/styles.tsx
@@ -6,11 +6,25 @@ export default StyleSheet.create({
         margin: 10,
         justifyContent: 'center',
     },
+    ListView: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+    },
     ListText: {
+        flex: 8,
         margin: 5,
         fontSize: 17,
     },
+    ListDateText: {
+        flex: 2,
+        textAlign: 'right',
+    },
     DivierView: {
         borderWidth: 1,
+        borderColor: 'gray',
+    },
+    ListDoneText: {
+        textDecorationLine: 'line-through',
     },
 });
